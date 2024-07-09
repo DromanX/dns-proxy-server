2027bb8a7169faf08a8738444593c8e30a25086b
DromanX <dromanx7@gmail.com>
2024-07-09 18:26:20 +0300

    Исправлена работа условия в config.c

--- src/config.c
+++ src/config.c
@@ -5,8 +5,7 @@
 
 #define MAX_LINE_LENGTH 256
 
-void readConfig(const char *filename, DnsProxyConfig *config)
-{
+void readConfig(const char *filename, DnsProxyConfig *config) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Ошибка открытия файла конфигурации");
@@ -18,24 +17,25 @@ void readConfig(const char *filename, DnsProxyConfig *config)
         char *key = strtok(line, "=");
         char *value = strtok(NULL, "\n");
 
-        if (*key == '\n' || value == NULL) {
-            if (strcmp(key, "dns_server_ip") == 0) {
-                strncpy(config->upstream_dns_server_ip, value, INET_ADDRSTRLEN);
-            } else if (strcmp(key, "blacklist_response_type") == 0) {
-                strncpy(config->blacklist_response_type, value, sizeof(config->blacklist_response_type));
-            } else if (strcmp(key, "fixed_ip") == 0) {
-                strncpy(config->fixed_ip, value, INET_ADDRSTRLEN);
-            } else if (strcmp(key, "blacklist") == 0) {
-                char *domain = strtok(value, ",");
-                while (domain != NULL) {
-                    config->blacklist = realloc(config->blacklist, sizeof(char *) * (++config->blacklist_size));
-                    if (config->blacklist == NULL) {
-                        perror("Невозможно выделить память");
-                        exit(EXIT_FAILURE);
-                    }
-                    config->blacklist[config->blacklist_size - 1] = strdup(domain);
-                    domain = strtok(NULL, ",");
+        if (*key == '\n' || value == NULL)
+            continue;
+
+        if (strcmp(key, "dns_server_ip") == 0) {
+            strncpy(config->upstream_dns_server_ip, value, INET_ADDRSTRLEN);
+        } else if (strcmp(key, "blacklist_response_type") == 0) {
+            strncpy(config->blacklist_response_type, value, sizeof(config->blacklist_response_type));
+        } else if (strcmp(key, "fixed_ip") == 0) {
+            strncpy(config->fixed_ip, value, INET_ADDRSTRLEN);
+        } else if (strcmp(key, "blacklist") == 0) {
+            char *domain = strtok(value, ",");
+            while (domain != NULL) {
+                config->blacklist = realloc(config->blacklist, sizeof(char *) * (++config->blacklist_size));
+                if (config->blacklist == NULL) {
+                    perror("Невозможно выделить память");
+                    exit(EXIT_FAILURE);
                 }
+                config->blacklist[config->blacklist_size - 1] = strdup(domain);
+                domain = strtok(NULL, ",");
             }
         }
     }
